ec4ed7e71eae3f298a9a312abafeddaca492b98e
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-02-17 22:41:08 +0530

    day 3 comments

---
+++ cpp-day-3/comments.cpp
@@ -0,0 +1,12 @@
+/*
+this is a c++ program to 
+introduce comments this is called a multiline comment
+*/
+
+#include <iostream>
+#include <string>
+int main() {
+	std::string stringData = "This is c++"; //this is a single line comment this variable store sting value 
+	std::cout << stringData << std::endl;
+	return 0;
+}
