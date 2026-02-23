aa01055b2eaf1271aa70cf07ce6c5801cf3fcd12
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-02-23 12:39:29 +0530

    break and continue

---
+++ 04-cpp-section-4/07 break and continue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+	int num = 0;
+	while (num <= 10) {
+		if (num == 5) {
+			num++;
+			//continue; // this will skip 5
+			break; //stps when num become 5    (0 to 4)
+			
+		}
+		std::cout << num << std::endl;
+		num++;
+	}
+
+	return 0;
+}
