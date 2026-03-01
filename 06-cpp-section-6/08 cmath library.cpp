122302e9e6e3840d124f4bb8475f5e7b2fe489cc
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-01 18:22:43 +0530

    cmath

---
+++ 06-cpp-section-6/08 cmath library.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<cmath>
+
+int main() {
+
+	int powerVar = std::pow(2, 3);
+	int squtVar = std::sqrt(25);
+	int ceilVar = std::ceil(4.14);
+	int floorVar = std::floor(4.12);
+
+	std::cout<< "powerVar : " << powerVar <<std::endl;
+	std::cout << "squtVar : " << squtVar << std::endl;
+	std::cout << "ceilVar : " << ceilVar << std::endl;
+	std::cout << "floorVar : " << floorVar << std::endl;
+
+	return 0;
+}
+
+
+
