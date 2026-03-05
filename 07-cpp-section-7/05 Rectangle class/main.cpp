a05217b2ec930574be88c5d84a59fafe70e86759
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-05 23:18:19 +0530

    rectangle class v2

--- 07-cpp-section-7/05 Rectangle class/main.cpp
+++ 07-cpp-section-7/05 Rectangle class/main.cpp
@@ -4,25 +4,30 @@
 int main() {
 
 	Rectangle r1;
-	Rectangle r2;
+	Rectangle r2(1,1);
 
 
-	r1.setlength(1);
-	r1.setWidth(1);
+	std::cout << "with out Parameter" << std::endl;
+	std::cout << "  r1 : " << "length : " << r1.getlength() << "  width : " << r1.getWidth() << "  area :  " << r1.area() << "  perimeter  : " << r1.perimeter() << std::endl;
+	std::cout << "  r2 : " << "length : " << r2.getlength() << "  width : " << r2.getWidth() << "  area :  " << r2.area() << "  perimeter  : " << r2.perimeter() << std::endl;
+	std::cout << std::endl;
 
-	r2.setlength(2);
-	r2.setWidth(2);
+	
 
-	r1.getlength();
-	r1.getWidth();
 
-	r2.getlength();
-	r2.getWidth();
+	std::cout << "after setting" << std::endl;
+
+	r1.setlength(2);
+	r1.setWidth(2);
+
+	r2.setlength(3);
+	r2.setWidth(3);
+
 
 
 	std::cout << "  r1 : " << "length : " << r1.getlength() << "  width : "<< r1.getWidth() << "  area :  "<< r1.area() << "  perimeter  : " << r1.perimeter() << std::endl;
 	std::cout << "  r2 : " << "length : " << r2.getlength() << "  width : " << r2.getWidth() << "  area :  " << r2.area() << "  perimeter  : " << r2.perimeter() << std::endl;
-
+	std::cout << std::endl;
 
 	return 0;
 }
