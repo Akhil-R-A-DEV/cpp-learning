d0ca934823eb6f86d27706fdf7723a1972bcfe7e
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-13 04:09:08 +0530

    polymorphism

---
+++ 11-cpp-section-11/02 polymorphism/main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include "Animal.h"
+#include "Dog.h"
+
+int main() {
+
+	//Animal cow("smallCow", 62);
+	//Dog dog1("big dog", 20, "breed1");
+
+	Animal* dogptr = new Dog("dog1", 20, "breed1");
+
+	std::cout << " Noise : " << dogptr->makeNoise() << std::endl;
+	std::cout << " eat : " << dogptr->eat() << std::endl;
+	
+
+	delete dogptr;
+	dogptr = nullptr;
+
+
+
+
+
+	//std::cout << " Name : " << cow.getName() << std::endl;
+	//std::cout << " Weight : " << cow.getWeight() << std::endl;
+	//std::cout << " Noise :  " << cow.makeNoise() << std::endl;
+
+	//std::cout << "-------------------------------------------------" << std::endl;
+
+	//std::cout << " Name : " << dog1.getName() << std::endl;
+	//std::cout << " Weight : " << dog1.getWeight() << std::endl;
+	//std::cout << " Breed : " << dog1.getBreed() << std::endl;
+	//dog1.digHole();
+	//std::cout << " Noise : " << dog1.makeNoise() << std::endl;
+	//dog1.catchCat();
+
+	return 0;
+}
