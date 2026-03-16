d6b2c276b22948a395c20dbffb63bbf7dca2f81b
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-17 01:58:54 +0530

    algorithm

---
+++ 12-cpp-section-12/07 algorithm , count and replace.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<algorithm>
+#include<vector>
+
+void fillVector(std::vector<int>& myVector);
+void printVector(const std::vector<int>& myVector);
+void countFives(const std::vector<int>& myVector);
+
+
+
+int main() {
+	
+	std::vector<int>myVector;
+	fillVector(myVector);
+	printVector(myVector);
+	countFives(myVector);
+
+	replace(myVector.begin(), myVector.end(), 5, 99);
+	printVector(myVector);
+	countFives(myVector);
+
+	return 0;
+}
+
+
+void fillVector(std::vector<int>& myVector) {
+	std::srand(time(nullptr));
+	for (int i = 0; i < 20; i++) {
+		myVector.push_back(rand() % 5 + 1);
+	}
+}
+void printVector(const std::vector<int>& myVector) {
+	for (int a : myVector) {
+		std::cout << a << " ";
+	}
+	std::cout << std::endl;
+}
+void countFives(const std::vector<int>& myVector) {
+	int countOfFive = count(myVector.begin(), myVector.end(), 5);
+	std::cout << "count of 5 : " << countOfFive << std::endl;
+}
+
